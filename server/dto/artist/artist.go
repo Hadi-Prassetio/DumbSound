e7a144f06b9428d00dd7ee0f8d806d27086bdcc5
hadi prassetio <hadiprassetio516@gmail.com[C>
2022-11-10 20:22:19 +0700

    delete server

--- server/dto/artist/artist.go
+++
@@ -1,8 +0,0 @@
-package artistdto
-
-type RequestCreateArtist struct {
-	Name        string `json:"name"  validate:"required"`
-	Old         int    `json:"old"  validate:"required"`
-	Role        string `json:"role"  validate:"required"`
-	StartCareer int    `json:"start_career"  validate:"required"`
-}
--- server/dto/song/song.go
+++
@@ -1,9 +0,0 @@
-package songdto
-
-type RequestSong struct {
-	Title    string `jsom:"title" validate:"required"`
-	Image    string `json:"image"`
-	Year     int    `json:"year" validate:"required"`
-	Song     string `json:"song"`
-	ArtistID int    `json:"artist_id"`
-}
